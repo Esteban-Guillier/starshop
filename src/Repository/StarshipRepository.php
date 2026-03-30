564bb8d14524e493d7a15941c93e075a25d22dea
Esteban-Guillier <outil.info.este@gmail.com>
2026-03-30 13:52:20 +0200

    Itinéraires plus sophistiqués : conditions requises, caractères génériques et plus encore

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -43,6 +43,36 @@ class StarshipRepository
                 'Operational'
             ),
         ];
+
+    }
+    public function find(int $id): ?Starship
+        {
+            $this->logger->info(sprintf('Fetching starship with id %d from repository', $id));
+
+            return match ($id) {
+                1 => new Starship(
+                    1,
+                    'Millenium Falcon',
+                    'YT-1300 light freighter',
+                    'Fildrong',
+                    'Operational'
+                ),
+                2 => new Starship(
+                    2,
+                    'X-Wing',
+                    'T-65B X-wing starfighter',
+                    'Luke Skywalker',
+                    'Operational'
+                ),
+                3 => new Starship(
+                    3,
+                    'TIE Fighter',
+                    'Twin Ion Engine/Ln Starfighter',
+                    'Darth Vader',
+                    'Operational'
+                ),
+                default => null
+            };
     }    
 
 }
