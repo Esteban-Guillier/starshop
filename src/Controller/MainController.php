e05e78d6f29d2edf031a3c76f8ea11d349846ad1
Esteban-Guillier <outil.info.este@gmail.com>
2026-03-31 09:55:46 +0200

    Méthodes de modèles intelligentes et dynamisation de la conception

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route]
+    #[Route('/home', name:'app_home')]
     public function HomePage(StarshipRepository $starshipRepository) : Response
     {
         $ships = $starshipRepository->findAll();
